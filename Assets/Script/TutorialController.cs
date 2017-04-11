3e4321056067210780919cdcaa8ffb40779dcf4e
S1321158\1321158 <klsh72@yahoo.co.jp>
2017-04-12 01:57:28 +0900

    ゲーム説明の追加

---
+++ Assets/Script/TutorialController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour {
+
+	[SerializeField] private List<GameObject> img = new List<GameObject>();
+
+}
